fb6c249b5e9de86807ff15b2257f240b565e936f
sapna21 <malaviyasapna@gmail.com>
2023-10-04 15:18:20 -0400

    unused import removed from SearchTest.java

--- src/test/java/com/tutorialsninja/qa/testcases/SearchTest.java
+++ src/test/java/com/tutorialsninja/qa/testcases/SearchTest.java
@@ -1,6 +1,5 @@
 package com.tutorialsninja.qa.testcases;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
